01ab855b3ba18c90ee5b03d40111e99cf972681b
gorbunovakris4 <51321340+gorbunovakris4@users.noreply.github.com>
2023-07-08 00:08:16 +0300

    Update Context

--- EntrepreneurshipSchoolBackend/Models/ApiDbContext.cs
+++ EntrepreneurshipSchoolBackend/Models/ApiDbContext.cs
@@ -6,9 +6,30 @@ public class ApiDbContext : DbContext
 {
     public ApiDbContext(DbContextOptions<ApiDbContext> options): base(options)
     {
-
     }
+    
+    public DbSet<AssessmentsType> AssessmentsTypes { get; set; }
+    public DbSet<Assessments> Assessments { get; set; }
+    public DbSet<Attend> Attends { get; set; }
+    public DbSet<Claim> Claim { get; set; }
+    public DbSet<ClaimStatus> ClaimStatuses { get; set; }
+    public DbSet<ClaimType> ClaimTypes { get; set; }
+    public DbSet<Group> Groups { get; set; }
+    public DbSet<Learner> Learner { get; set; }
     public DbSet<Lesson> Lessons { get; set; }
+    public DbSet<Lot> Lot { get; set; }
+    public DbSet<Relate> Relates { get; set; }
+    public DbSet<Solution> Solutions { get; set; }
+    public DbSet<Task> Tasks { get; set; }
+    public DbSet<TaskType> TaskTypes { get; set; }
+    public DbSet<Transaction> Transactions { get; set; }
+    public DbSet<TransactionType> TransactionTypes { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql(Environment.GetEnvironmentVariable("DB_CONNECTION_STRING"));
+    }
+    
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
