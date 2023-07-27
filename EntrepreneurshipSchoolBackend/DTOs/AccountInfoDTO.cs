385e87a13b76a1c9850d05f9fbdc0de6733d76d8
gorbunovakris4 <51321340+gorbunovakris4@users.noreply.github.com>
2023-07-27 09:06:10 +0300

    Add DTOs for Account Controller

---
+++ EntrepreneurshipSchoolBackend/DTOs/AccountInfoDTO.cs
@@ -0,0 +1,31 @@
+﻿namespace EntrepreneurshipSchoolBackend.DTOs
+{
+    public class AccountInfo
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public List<int> teamNumber { get; set; }
+        public string role { get; set; }
+        public int balance { get; set; }
+    }
+
+    public class ExtendedAccountInfo
+    {
+        public int id { get; set; }
+        public string fullName { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+        public string? messenger { get; set; }
+        public bool? gender { get; set; }
+        public List<int> teamNumber { get; set; }
+        public string role { get; set; }
+        public int balance { get; set; }
+    }
+
+    public class AccountShortenInfo
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+}
