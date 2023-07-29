383033b6bdfc7847ce229008cebc34cbaa0c3ae3
gorbunovakris4 <51321340+gorbunovakris4@users.noreply.github.com>
2023-07-30 00:25:56 +0300

    Add DTOs for lessons

---
+++ EntrepreneurshipSchoolBackend/DTOs/LessonRequestDTO.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+
+namespace EntrepreneurshipSchoolBackend.DTOs
+{
+    public class LessonRequest
+    {
+        public int id { set; get; }
+        public int number { set; get; }
+        public string title { set; get; }
+        public string description { set; get; }
+        public string date { set; get; }
+        public string? presLink { set; get; }
+        public string? videoLink { set; get; }
+        public int? homeworkId { set; get; }
+        public int? testId { set; get; }
+    }
+
+    public class LessonComplexRequest
+    {
+        public int? lessonNumber { set; get; }
+
+        public string? lessonTitle { set; get; }
+        public string? sortProperty { set; get; }
+        public string? sortOrder { set; get; }
+        public bool pageable { set; get; }
+        public int page { set; get; } = 1;
+        public int pageSize { set; get; } = 10;
+    }
+
+}
